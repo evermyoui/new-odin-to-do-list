f5e25d131f4ea180dbb32b5d4ab922818275313c
said <sexbythewind@gmail.com>
2026-01-31 23:14:45 +0800

    storage functions

--- src/factories/storage.js
+++ src/factories/storage.js
@@ -1,3 +1,6 @@
+import { todoDependencies } from "../managers/projectManager";
+import { Project } from "./project";
+import { Todo } from "./todo";
 const storageAvail = (type) => {
     try {
         const storage = window[type];
@@ -8,5 +11,33 @@ const storageAvail = (type) => {
         return true;
     }catch(e){
         return false;
+    }
+}
+const populateStorage = () => {
+    if (storageAvail("localStorage")){
+        localStorage.setItem("todoDatas", JSON.stringify(todoDependencies.projects.filter(project => project.id !== "default")));
+    }
+}
+
+const populateDependencies = () => {
+    if (storageAvail("localStorage")){
+        if (localStorage.getItem("todoDatas")== null){
+            populateStorage();
+        }else {
+            const savedDatas = JSON.parse(localStorage.getItem("todoDatas"));
+            // 1 for keeping the default project.
+            todoDependencies.projects.length = 1;
+
+            savedDatas.forEach(p => {
+                const project = new Project(p.title);
+                project.id = p.id;
+                project.todos = p.todos.map(t => {
+                    const todo = new Todo(t.title, t.description, t.priority);
+                    todo.id = t.id;
+                    return todo;
+                });
+                todoDependencies.projects.push(project);
+            })
+        }
     }
 }
